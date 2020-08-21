9de9783631dcf4e12f37a88a69c5365ae544501d
Brus Palaj <brus.palaj71@gmail.com>
2020-08-21 18:33:44 -0400

    created api, controller, and model for jobs - now trying to link Material Table components to backend and connect onchange and onclick functions to the Material Table UI

---
+++ routes/controllers/jobs.js
@@ -0,0 +1,49 @@
+// get reference to DB
+const db = require("../../models");
+const { sequelize } = require("../../models");
+
+module.exports = {
+  newJob: (req, res, next) => {
+    // create user in db
+    db.Jobs.create({
+      email: req.body.email,
+      company: req.body.company,
+      jobTitle: req.body.jobTitle,
+      location: req.body.location,
+      status: req.body.status,
+      notes: req.body.notes,
+    })
+
+      // redirect to login
+      .then((jobs) => res.json(jobs))
+      .catch((err) => {
+        res.status(401);
+        next(err);
+      });
+  },
+
+  findAll: (req, res, next) => {
+    db.Jobs.findAll({ order: [["createdAt", "DESC"]] })
+      .then((jobs) => res.json(jobs))
+      .catch((err) => {
+        res.status(401);
+        next(err);
+      });
+  },
+
+  //   findTotalJobNumber: (req, res, next) => {
+  //     db.Jobs.findAll({
+  //       where: { email: req.params.email },
+  //       attributes: [
+  //         "donatedItemCategory",
+  //         [sequelize.fn("sum", sequelize.col("quantity")), "total"],
+  //       ],
+  //       group: ["Postings.donatedItemCategory"],
+  //     })
+  //       .then((donations) => res.json(donations))
+  //       .catch((err) => {
+  //         res.status(401);
+  //         next(err);
+  //       });
+  //   },
+};
