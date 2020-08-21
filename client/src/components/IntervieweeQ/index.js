77d1da29f56e5e35b4b81c3bb152216a65224a51
tango <sm91wilson@gmail.com>
2020-08-21 18:39:15 -0400

    adding faq page

---
+++ client/src/components/IntervieweeQ/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class intervieweeQ extends Component {
+    render(){
+        return(
+            <div className='col-4'>
+
+            </div>
+        )
+}
+}
---
+++ client/src/pages/FAQ/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => {
+    return(
+        <div className='container'>
+            <h1 className='row'>
+                Frequently Asked Questions
+            </h1>
+            <div className='row'>
+                <interviewerQ />
+                <intervieweeQ />
+            </div>
+        </div>
+    )
+}
