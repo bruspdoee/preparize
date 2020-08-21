2e6fd6438fe2140cf05bb709af5bf50db2bf20e8
Brus Palaj <brus.palaj71@gmail.com>
2020-08-21 12:59:45 -0400

    added a customizable job board ui to home page - next up need to link all of it to Backend

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,21 +1,88 @@
 import React from "react";
-import style from "./style.module.css";
+import MaterialTable from "material-table";
 
-export default ( props ) => {
-    
-    return (
-        <>
-        <div className={`spin ${style.container}`} >
-            Home
-        </div>
-        <div className={style.user}>
-            <h5>user:</h5>
-            { Object.keys(props.user)
-                .map( (field, i) => 
-                    <p key={i}> <strong>{field}</strong>: {props.user[field]}</p>) 
-            }
-        </div>
-        
-        </>
-    )
+export default function UserJobTable() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: "Company", field: "company" },
+      { title: "Job Title", field: "jobTitle" },
+      { title: "Location", field: "location" },
+      {
+        title: "Status",
+        field: "status",
+        lookup: {
+          1: "Applied to job",
+          2: "1st Interview",
+          3: "2nd Interview",
+          4: "3rd Interview",
+          5: "4th Interview",
+          6: "Final Interview",
+          7: "Offer",
+          8: "Accepted",
+          9: "Rejected",
+        },
+      },
+      { title: "Notes", field: "notes" },
+    ],
+    data: [
+      {
+        company: "Google",
+        jobTitle: "Software Engineer",
+        location: "New York, NY",
+        status: 3,
+        notes: "1st I/V went really well, awaiting next steps",
+      },
+      {
+        company: "YouTube",
+        jobTitle: "Junior Software Engineer",
+        location: "New York, NY",
+        status: 1,
+      },
+    ],
+  });
+
+  return (
+    <MaterialTable
+      title={"Brus" + "'s Job Board"}
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              if (oldData) {
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data[data.indexOf(oldData)] = newData;
+                  return { ...prevState, data };
+                });
+              }
+            }, 600);
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve) => {
+            setTimeout(() => {
+              resolve();
+              setState((prevState) => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
 }
